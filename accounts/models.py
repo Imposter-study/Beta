635338a536faba326aec5ae30506701b841047cb
Heumhub <jdh6983@naver.com>
2025-06-06 20:50:43 +0900

    🚧 Wip: accounts models.py
    모델 조금 만든거 올림

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    GENDER_CHOICES = [("M", "남자"), ("F", "여자"), ("O", "기타")]
+    
+    username = models.CharField(max_length=20, unique=True)
+    nickname = models.CharField(max_length=20, unique=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=1, blank=True, null=True
+        )
+    age = models.IntegerField(blank=True, null=True)
+    introduce = models.TextField(blank=True, null=True)
+    # profilepicture = models.ImageField(blank=True, null=True)
+    # follower = models.ManyToManyField(
+    #    "self", symmetrical=False, related_name="following", blank=True
+    # )
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("signup/", views.UserCreateView.as_view()),
 ]
-

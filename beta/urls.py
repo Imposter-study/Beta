813e76022b932958576c0c3998cdc604fac9056e
SH_kim <dal2018@naver.com>
2025-06-08 19:24:43 +0900

    ✨ Feat: add accounts swagger
    accounts 의 회원기능의 swagger 작성

--- beta/urls.py
+++ beta/urls.py
@@ -18,7 +18,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    # SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/accounts/", include("accounts.urls")),
+    # YOUR PATTERNS
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Optional UI:
+    path(
+        "api/v1/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    # path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]

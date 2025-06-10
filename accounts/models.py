160e3c020e9965173dc2e4a83f716cdf0a862566
Heumhub <jdh6983@naver.com>
2025-06-10 16:17:53 +0900

    ✏️ Fix: Remove unused code
    중복 코드 제거, 코드 가독성 향상 비교

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,12 @@ class User(AbstractUser):
     gender = models.CharField(default="O", choices=GENDER_CHOICES, max_length=1)
     age = models.IntegerField(default=0)
     introduce = models.TextField(blank=True, null=True)
+    is_deactivated = models.BooleanField(
+        default=False
+    )  # 이 유저가 탈퇴한 상태인지를 나타내는 값
+    deactivated_at = models.DateTimeField(
+        null=True, blank=True
+    )  # 탈퇴 요청한 시간 저장용 필드
 
     # profilepicture = models.ImageField(blank=True, null=True)
     # follower = models.ManyToManyField(

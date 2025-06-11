ee093bade36b4ea7e4d055c2cb591e889f120cf9
jeil <sjl01002@gmail.com>
2025-06-11 13:38:21 +0900

    🎨 Format: black
    - black formatter를 사용한 자동 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.UserCreateView.as_view()),
-    path('signin/', views.LoginView.as_view()),
-    path('signout/', views.LogoutView.as_view()),
-    path('password/', views.PasswordChangeView.as_view()),
-    path('delete/', views.DeactivateAccountView.as_view()),
+    path("signup/", views.UserCreateView.as_view()),
+    path("signin/", views.LoginView.as_view()),
+    path("signout/", views.LogoutView.as_view()),
+    path("password/", views.PasswordChangeView.as_view()),
+    path("delete/", views.DeactivateAccountView.as_view()),
     path("<str:nickname>/", views.UserProfileView.as_view()),
 ]

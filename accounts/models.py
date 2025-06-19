4355dfa615f18636755edcfddffe00930205803a
Heumhub <jdh6983@naver.com>
2025-06-20 02:40:23 +0900

    ✨ Feat: auto-nickname
    닉네임이 없다면 자동 생성, 중복 닉네임 방지

--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 from datetime import timedelta
-
+import random
 
 class User(AbstractUser):
     GENDER_CHOICES = [("M", "남자"), ("F", "여자"), ("O", "기타")]
+    WORD_POOL = [
+        "red", "blue", "yellow", "purple", "green",
+        "dog", "bird", "monkey", "tiger", "cow"
+    ]
 
     username = models.CharField(max_length=20, unique=True)
     nickname = models.CharField(max_length=20, unique=True)
@@ -19,6 +23,21 @@ class User(AbstractUser):
     # follower = models.ManyToManyField(
     #    "self", symmetrical=False, related_name="following", blank=True
     # )
+    def save(self, *args, **kwargs):  # 자동 닉네임 생성 추가
+        if not self.nickname:
+            self.nickname = self.generate_random_nickname()
+        super().save(*args, **kwargs)
+
+    def generate_random_nickname(self):  # 랜덤 닉네임 생성기
+        while True:
+            nickname = (
+                f"{random.choice(self.WORD_POOL)}_"
+                f"{random.choice(self.WORD_POOL)}_"
+                f"{random.randint(100, 999)}"
+            )
+            if not User.objects.filter(nickname=nickname).exists():
+                return nickname
+
     def mark_as_deactivated(self):
         self.is_active = False
         self.save()

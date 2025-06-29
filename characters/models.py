5613ddbc0dabf8361ec7f101626c970e9a494cdd
jeil <sjl01002@gmail.com>
2025-06-29 19:13:55 +0900

    🩹 Fix: method
    fix: 들여쓰기 수정

--- characters/models.py
+++ characters/models.py
@@ -93,12 +93,11 @@ class ConversationHistory(models.Model):
         verbose_name = "대화 내역"
         verbose_name_plural = "대화 내역들"
 
+    def save(self, *args, **kwargs):
+        if self.chat_history and not self.last_message:
+            self.last_message = self.chat_history[-1].get("content", "")[:50]
 
-def save(self, *args, **kwargs):
-    if self.chat_history and not self.last_message:
-        self.last_message = self.chat_history[-1].get("content", "")[:50]
-
-    super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.character.name} - {self.title}"

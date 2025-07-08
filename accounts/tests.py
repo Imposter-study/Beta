8226076f8e5d59b763719f33f103ec40a69c520b
Drill <sdi6566@gmail.com>
2025-07-08 22:36:47 +0900

    ✅ Add: accounts signup success test case
    회원가입 테스트 생성 및 성공 케이스 추가

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+# 회원 가입 테스트
+class SignUpTest(TestCase):
+
+    # 회원가입 성공 테스트
+    def test_signup_success(self):
+        # 회원가입 데이터 기입
+        signup_data = {
+            "username": "testuser1",
+            "password": "1q2w3e4r!",
+            "password_confirm": "1q2w3e4r!",
+            # 선택 필드 예시 (필요시 추가)
+            # "nickname": "테스트닉네임",
+            # "birth_date": "2000-01-01",
+            # "gender": "M",
+            # "introduce": "안녕하세요"
+        }
+
+        response = self.client.post(
+            "/api/v1/accounts/signup/", signup_data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 201)
+
+        User = get_user_model()
+        self.assertTrue(User.objects.filter(username="testuser1").exists())
+        print("\n회원가입 성공\n")

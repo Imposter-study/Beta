dd71c29dbd5d41844082674ea00417b3f31ff60f
Drill <sdi6566@gmail.com>
2025-07-09 17:16:45 +0900

    ✅ Add: signuptest method add
    1.유저 중복 2.비밀번호 검증 3.비밀번호 일치 테스트 추가

--- accounts/tests/tests_views.py
+++ accounts/tests/tests_views.py
@@ -32,3 +32,46 @@ class SignUpTest(TestCase):
         User = get_user_model()
         self.assertTrue(User.objects.filter(username="testuser1").exists())
         print("\n회원가입 성공")
+
+    # 유저네임 중복 테스트
+    def test_signup_username_duplicate(self):
+        signup_data = {
+            "username": "testuser1",
+            "password": "1q2w3e4r!",
+            "password_confirm": "1q2w3e4r!",
+        }
+
+        response = self.client.post(
+            "/api/v1/accounts/signup/", signup_data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400, "유저네임 중복")
+        print("\n유저네임 중복")
+
+    # 비밀번호 검증 테스트
+    def test_signup_password_length(self):
+        signup_data = {
+            "username": "testuser2",
+            "password": "1q2w4r!",
+            "password_confirm": "1q2w4r!",
+        }
+
+        response = self.client.post(
+            "/api/v1/accounts/signup/", signup_data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+        print("\n비밀번호 검증")
+
+    # 비밀번호 일치 테스트
+    def test_signup_password_mismatch(self):
+        signup_data = {
+            "username": "testuser2",
+            "password": "1q2w3e4r!",
+            # 다른 비밀번호
+            "password_confirm": "2w3e4r",
+        }
+
+        response = self.client.post(
+            "/api/v1/accounts/signup/", signup_data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400, "비밀번호 일치")
+        print("\n비밀번호 일치")

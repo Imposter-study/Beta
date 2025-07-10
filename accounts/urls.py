cb7d90282f3221dd848beadb6be273d8a24f36a2
Drill <sdi6566@gmail.com>
2025-07-10 20:07:39 +0900

    ♻️ Refactore: accounts urls
    유저 CRUD를 router를 이용해서 리팩토링

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-from . import views
-from rest_framework_simplejwt.views import TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
+from . import views
+
+router = DefaultRouter()
+router.register("", views.UserViewSet, basename="user")
 
 urlpatterns = [
-    path("signup/", views.UserCreateView.as_view()),
     path("signin/", views.LoginView.as_view()),
     path("signout/", views.LogoutView.as_view()),
     path("password/", views.PasswordChangeView.as_view()),
-    path("delete/", views.DeactivateAccountView.as_view()),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("kakao/login/", views.KakaoLogin.as_view(), name="kakao_login"),
     path("kakao/redirect/", views.kakao_redirect, name="kakao_redirect"),
@@ -26,11 +28,11 @@ urlpatterns = [
     path(
         "chat_profiles/<uuid:chatprofile_uuid>/", views.ChatProfileDetailView.as_view()
     ),
-    # 프로필조회
-    path("my_profile/", views.MyProfileView.as_view()),
-    path("<uuid:uuid>/", views.UserProfileView.as_view()),
 ]
 
 # 개발용 미디어 파일 제공 설정
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# UserViewSet 회원 관련 url (회원가입, 회원탈퇴, 회원정보 수정, 회원정보 조회)
+urlpatterns += router.urls
